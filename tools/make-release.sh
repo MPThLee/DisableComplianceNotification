89425715a5b38cd17323840d84faa2d24313f495
MPThLee <mpthlee@gmail.com>
2022-07-14 18:06:29 +0900

    build: :pushpin: archive version

--- tools/make-release.sh
+++ tools/make-release.sh
@@ -7,7 +7,7 @@ mc_version=$(./gradlew properties | grep ^minecraft_version: | cut -d ':' -f2 | 
 ./gradlew build
 
 mkdir release/
-cp forge/build/libs/$name-$version.jar release/$name-$version-$mc_version-forge.jar
-cp fabric/build/libs/$name-$version.jar release/$name-$version-$mc_version-fabric.jar
-cp quilt/build/libs/$name-$version.jar release/$name-$version-$mc_version-quilt.jar
+cp forge/build/libs/$name-$version-$mc_version-forge.jar release/
+cp fabric/build/libs/$name-$version-$mc_version-fabric.jar release/
+cp quilt/build/libs/$name-$version-$mc_version-quilt.jar release/
 
